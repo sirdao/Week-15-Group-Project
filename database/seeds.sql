2353512d38c423a79179c86a91ca8ccc1546a324
Phillip Ramos <phillipramos@Phillips-MacBook-Pro.local>
2016-07-19 22:47:33 -0400

    adding scschema and seeds file to show database and info

---
+++ database/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO WHATS_AROUND (Places, score, reviews)
+VALUES ('Subway', '3', 'Great sandwiches and always fast service'),
+('Publix', '3', 'Lots of different options of food to choose from that are made fresh daily My main reason for giving it a score of only three is due to the horrible wait times and how busy this location gets during peak times'),
+('Europe Bakery','5','Wonderful location with great home cooked dishes My order was served in a decent time and prices were very generous'),
+('Burger King','4','Close to campus and very fast service They also have a decent price menu and a good selection of food to pick from'),
+('Wawa','5','Very close to campus and fast service Great selection of food that is ordered on a computer and made fresh for you')
